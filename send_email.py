fef68c27cf55c766c56fd3b5cb98eaa7872e2477
ManojSharma <sharma.manoj84@yahoo.co.in>
2025-02-20 18:58:26 +0530

    secured email account password

--- send_email.py
+++ send_email.py
@@ -1,12 +1,17 @@
 import smtplib
 import ssl
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "sharma.manoj.aayush2303@gmail.com"
-    password = "ieil hvox buhd mxiu"
+    password = os.environ.get("PASSWORD")
     receiver = "sharma.manoj.aayush2303@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
